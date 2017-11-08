51a5a243118676b9d2bde79d00744745faaf9c08
Rich Carlsen <ricarlse@microsoft.com>
2017-11-08 12:26:13 -0800

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=AzureBotServiceMotoGP46-Web-Deploy.pubxml -p:Password=rtoeMZDQiccjrY3WwHdCpH5hoeRnleHltvzflWfEJl9gmE00uQgdZ6fH5wKr
+
